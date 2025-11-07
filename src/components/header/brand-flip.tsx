41ee3187bfc3b6f05b18ad9c0348f1a26a671187
redaezziani <klausdev2@gmail.com>
2025-11-07 11:41:08 +0100

    fix: adjust interval timing and component dimensions for improved performance and layout

--- src/components/header/brand-flip.tsx
+++ src/components/header/brand-flip.tsx
@@ -4,25 +4,25 @@ export default function BrandFlip({ t }: { t: (key: string) => string }) {
   const [flipped, setFlipped] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => setFlipped((prev) => !prev), 1000);
+    const interval = setInterval(() => setFlipped((prev) => !prev), 10000);
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="relative w-24 h-24 [perspective:1000px]">
+    <div className="relative w-20 h-20 [perspective:1000px]">
       <div
         className={`relative w-full h-full transition-transform duration-700 [transform-style:preserve-3d] ${
           flipped ? '[transform:rotateY(180deg)]' : ''
         }`}
       >
         <div className="absolute inset-0 flex p-1 justify-center items-center rounded-full bg-red-800 text-white border border-white/80 font-bold text-xl  [backface-visibility:hidden]">
-          <div className="flex w-full h-full justify-center items-center p-2 text-center border border-white/50 rounded-full">
+          <div className="flex w-full -rotate-2 h-full justify-center items-center p-2 text-center border border-white/50 rounded-full">
             {t('fontbrandName')}
           </div>
         </div>
 
         <div className="absolute inset-0 flex p-1 justify-center items-center rounded-full bg-blue-800 text-white border border-white/80 font-semibold text-xl [transform:rotateY(180deg)] [backface-visibility:hidden]">
-          <div className="flex w-full h-full justify-center items-center p-2 text-center border border-white/50 rounded-full">
+          <div className="flex w-full -rotate-2 h-full justify-center items-center p-2 text-center border border-white/50 rounded-full">
             {t('backbrandName')}
           </div>
         </div>
