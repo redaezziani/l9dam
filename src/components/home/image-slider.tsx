867580862e7b7cb9b2e35e6e5bb83a596e5bee3b
redaezziani <klausdev2@gmail.com>
2025-11-09 12:11:53 +0100

    fix: clean up button styles and SVG attributes in image slider component

--- src/components/home/image-slider.tsx
+++ src/components/home/image-slider.tsx
@@ -37,32 +37,37 @@ const ImageSlider = () => {
       </div>
       <button
         onClick={scrollLeft}
-        className="absolute z-9999 left-1 h-10 top-1/2 win7-btn -translate-y-1/2 border! rounded-none! bg-[#b1aea1]  border-[#b1aea1] p-2 hover:bg-[#b1aea1]"
+        className="absolute z-9999 left-1 h-10 top-1/2  -translate-y-1/2 border! rounded-none! bg-[#b1aea1]  border-[#b1aea1] p-2 hover:bg-[#b1aea1]"
         type="button"
       >
         <svg
+          className="w-6"
+          fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="#908e85"
+          viewBox="0 0 24 24"
         >
-          <path d="M10.5 3L9.5 2 3.5 8l6 6 1-1-5-5z" />
+          <path
+            d="M16 5v2h-2V5h2zm-4 4V7h2v2h-2zm-2 2V9h2v2h-2zm0 2H8v-2h2v2zm2 2v-2h-2v2h2zm0 0h2v2h-2v-2zm4 4v-2h-2v2h2z"
+            fill="currentColor"
+          />
         </svg>
       </button>
       <button
         onClick={scrollRight}
-        className="absolute z-9999 right-1 h-10! win7-btn top-1/2 -translate-y-1/2 bg-[#b1aea1] border! rounded-none! border-[#b1aea1] p-2 hover:bg-[#b1aea1]"
+        className="absolute z-9999 right-1 h-10!  top-1/2 -translate-y-1/2 bg-[#b1aea1] border! rounded-none! border-[#b1aea1] p-2 hover:bg-[#b1aea1]"
         type="button"
       >
         <svg
+          className="w-6"
+          fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="#908e85"
+          viewBox="0 0 24 24"
         >
-          <path d="M5.5 13l1 1 6-6-6-6-1 1 5 5z" />
+          {' '}
+          <path
+            d="M8 5v2h2V5H8zm4 4V7h-2v2h2zm2 2V9h-2v2h2zm0 2h2v-2h-2v2zm-2 2v-2h2v2h-2zm0 0h-2v2h2v-2zm-4 4v-2h2v2H8z"
+            fill="currentColor"
+          />{' '}
         </svg>
       </button>
     </section>
