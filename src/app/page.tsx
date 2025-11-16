c091965b8a61398732b3781d9e61a8cc2c478ff3
redaezziani <klausdev2@gmail.com>
2025-11-16 17:45:44 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@ import { useTranslations } from 'next-intl';
 import HeroSection from '../components/home/hero-section';
 import BaseLayout from '../components/layout/base-layout';
 import AnimatedBtn from '../components/retroui/animated-btn';
+import Link from 'next/link';
 
 export default function HomePage() {
   const t = useTranslations('HomePage');
   return (
     <BaseLayout>
-      <div className=" fixed bottom-4 z-99999999">
-        <AnimatedBtn>{t('product.addToCart')}</AnimatedBtn>
-      </div>
+      <Link href={'/store'} className=" fixed bottom-2 z-99999999">
+        <img src={'/images/flip-bu.gif'} alt="flip-bu" className="w-28 h-28" />
+      </Link>
       <HeroSection />
     </BaseLayout>
   );
--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+// arabic text that indicates this is the store page
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center mt-10">
+        مرحبا بكم في صفحة المتجر
+      </h1>
+    </div>
+  );
 };
 
 export default page;
