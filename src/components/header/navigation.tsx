5c9e859e8ffa6f93d64d963df3f73a2514867d95
redaezziani <klausdev2@gmail.com>
2025-11-16 18:25:35 +0100

    feat: update cart link to include an SVG icon for better visual representation

--- src/components/header/navigation.tsx
+++ src/components/header/navigation.tsx
@@ -31,7 +31,22 @@ const Navigation = () => {
         </nav>
       </div>
       <span className=" flex gap-2 items-center">
-        <Link href="/cart"> {t('navLinks.cart')} </Link>
+        <Link href="/cart">
+          {' '}
+          <svg
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="20"
+            height="20"
+          >
+            {' '}
+            <path
+              d="M2 2h4v4h16v11H4V4H2V2zm4 13h14V8H6v7zm0 4h3v3H6v-3zm14 0h-3v3h3v-3z"
+              fill="currentColor"
+            />{' '}
+          </svg>{' '}
+        </Link>
         {'/'}
         <LangSwitcher />
       </span>
