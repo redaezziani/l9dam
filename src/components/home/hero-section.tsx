f850eb2a7f31833ec0111848602f4be57044283a
redaezziani <klausdev2@gmail.com>
2025-11-17 12:15:53 +0100

    feat: enhance hero section with storytelling and philosophy content

--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -1,22 +1,55 @@
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
+
 const HeroSection = () => {
   const t = useTranslations('HomePage');
 
   return (
-    <section className="w-full md:max-w-360 md:justify-between flex flex-col md:flex-row gap-6 px-4">
-      <span className=" relative flex justify-center items-center  max-w-96">
-        <img src={'/images/slider/image-2.png'} alt="product-preview" />
-      </span>
-      <div className="flex flex-2 flex-col gap-4">
-        <p className="text-sm text-[#4a403a] font-semibold leading-relaxed ">
-          {t('product.description')}
-        </p>
-        <div className=" flex gap-3 justify-start z-99999 items-center mt-6">
-          <Link href="/about-us" className="text-sm text-[#4a403aca] underline">
-            {t('product.moreDetails')}
-          </Link>
-        </div>
+    <section className="w-full md:max-w-360 py-4 flex flex-col gap-6 px-4">
+      <div className="text-sm text-[#4a403a] leading-relaxed space-y-4">
+        <p>{t('hero.story.intro')}</p>
+        <p>{t('hero.story.collaboration')}</p>
+        <p>{t('hero.story.qualities')}</p>
+        <p>{t('hero.story.message')}</p>
+        <Link
+          href="/about-us"
+          className="text-sm text-[#4a403aca] underline inline-block"
+        >
+          {t('hero.story.learnMore')}
+        </Link>
+      </div>
+
+      <div className="flex flex-col md:flex-row gap-6 md:justify-between">
+        <span className="relative flex justify-center items-center max-w-96">
+          <img src={'/images/slider/image-2.png'} alt="product-preview" />
+        </span>
+      </div>
+
+      <div className="text-sm text-[#4a403a] leading-relaxed space-y-4">
+        <p>{t('hero.philosophy.intro')}</p>
+
+        <p
+          dangerouslySetInnerHTML={{
+            __html: t('hero.philosophy.design')
+              .replace(
+                '"National Shoe"',
+                '<span class="bg-amber-300 h-1 px-1 ">"National Shoe"</span>',
+              )
+              .replace(
+                '"الحذاء الوطني"',
+                '<span class="bg-amber-300 h-1 px-1 ">"الحذاء الوطني"</span>',
+              ),
+          }}
+        />
+
+        <p>{t('hero.philosophy.simplicity')}</p>
+        <p>{t('hero.philosophy.collaboration')}</p>
+        <Link
+          href="/about-us"
+          className="text-sm text-[#4a403aca] underline inline-block"
+        >
+          {t('hero.philosophy.learnMore')}
+        </Link>
       </div>
     </section>
   );
