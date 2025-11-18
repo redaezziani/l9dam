44bee092a949b264030cd9fa6a39b4e1fd6da31d
redaezziani <klausdev2@gmail.com>
2025-11-18 14:45:26 +0100

    fix: adjust background size for improved visual consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,8 @@ export default async function RootLayout({ children }: Props) {
         className="flex flex-col  bg-[#fdefe5a8]  relative min-h-screen justify-start items-center"
         style={{ textAlign: textAlign as any }}
       >
-        <span className="absolute top-0 left-0 w-full h-full -z-10 pointer-events-none bg-[url('https://texturelabs.org/wp-content/uploads/Texturelabs_Paper_126S.jpg')] bg-repeat bg-top bg-size-[900px_900px] opacity-15 mix-blend-multiply animate-paperMove"></span>
+        <span className="absolute top-0 left-0 w-full h-full -z-10 pointer-events-none bg-[url('https://texturelabs.org/wp-content/uploads/Texturelabs_Paper_126S.jpg')] bg-repeat bg-top bg-size-[1000px_1000px] opacity-15 mix-blend-multiply animate-paperMove"></span>
 
-        {/* <span className="  z-999 bg-[#ff715115] w-full h-screen fixed " /> */}
         <NextIntlClientProvider>
           <Header />
           <main className="flex-1  w-full">{children}</main>
